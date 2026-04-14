fecaebf6b481bfd1c1986868a3330b68281cd921
ThiagoRobade <robade@live.com>
2026-04-14 17:30:28 -0300

    creates and configures the UserModel, UserRepository, UserDto, and UserService classes

---
+++ user-confirmation/user/src/main/java/dev/java10x/user/domain/UserModel.java
@@ -0,0 +1,48 @@
+package dev.java10x.user.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "TB_USERS")
+public class UserModel {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID userID;
+    private String name;
+    private String email;
+
+    public UUID getUserID() {
+        return userID;
+    }
+
+    public void setUserID(UUID userID) {
+        this.userID = userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ user-confirmation/user/src/main/java/dev/java10x/user/dto/UserDto.java
@@ -0,0 +1,13 @@
+package dev.java10x.user.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record UserDto(
+        @NotBlank
+        String name,
+        @Email
+        @NotBlank
+        String email
+) {
+}
